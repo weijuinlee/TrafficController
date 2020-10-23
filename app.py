27d5abdaf03234ca4d21bfb4efa3b55f4606ea18
weijuinlee <leewj96@gmail.com>
2020-10-23 09:20:25 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from rq.job import Job
+from flask_restful import reqparse
+from flask import Flask, abort, jsonify, request
+from path_plan import send_coordinates
+from redis_func import redis_conn, redis_queue
+
+app = Flask(__name__)
+
+@app.errorhandler(404)
+def resource_not_found(exception):
+    return jsonify(error=str(exception)), 404
+
+@app.route("/")
+def home():
+    return "Queue is running!"
+
+@app.route("/enqueue", methods=["POST"])
+def enqueue():
+
+    if request.method == "POST":
+        input_data = request.json
+        job = redis_queue.enqueue(send_coordinates,input_data)
+    return jsonify({"job_id": job.id})
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ traffic_control_func.py
@@ -0,0 +1,94 @@
+import requests
+import time
+import json
+import paho.mqtt.client as mqtt
+from ast import literal_eval
+
+broker_address="localhost"
+client = mqtt.Client("1") 
+client.connect(broker_address)
+dict_of_priority = {}
+dict_of_robot_shortest_path = {}
+dict_of_coordinates = {}
+
+def payload(robot,node):
+
+    node_coordinate = dict_of_coordinates[robot][node]
+    x_coordinate = str(node_coordinate['x'])
+    y_coordinate = str(node_coordinate['y'])
+
+    payload = '''{
+                "modificationType": "CREATE",
+                "abort": false,
+                "taskType": "GOTO",
+                "parameters": {},
+                "point": {
+                    "mapVerID": "b2a546f9-b7a1-4623-8c93-8a574b8db1f6",
+                    "positionName": "Showroom",
+                    "x": %s,
+                    "y": %s,
+                    "heading": 360
+                },
+                "totalTaskNo": 1,
+                "currTaskNo": 1
+    }'''%(x_coordinate,y_coordinate)
+
+    return payload
+
+def send_coordinates(input_data):
+
+    nodes_occupied = []
+
+    for robot in input_data['robot_id']:
+        
+        URL = "https://shortestpathfinderapi.herokuapp.com/robot/%s"%robot
+        r = requests.get(url = URL)
+        path_data = r.json()
+        coordinates = literal_eval(path_data['coordinates'])
+        dict_of_coordinates.update({robot:coordinates})
+        coordinates_of_shortest = literal_eval(path_data['shortest_path'])
+        dict_of_robot_shortest_path.update({robot:coordinates_of_shortest})
+        robot_priority = (path_data['priority'])
+        dict_of_priority.update({robot:robot_priority})
+        mqtt_payload = payload(robot,'0')
+        client.publish("%s/robot/task"%robot, mqtt_payload)
+
+    print(dict_of_robot_shortest_path)
+
+    if len(input_data['robot_id']) == 1:
+
+        print("One robot")
+
+    else:
+        
+        print("More than one robot")
+
+        for i in range(15):
+
+            list_of_robots = input_data['robot_id']
+
+            for robot in list_of_robots:
+
+                if dict_of_robot_shortest_path[robot] == []:
+
+                    list_of_robots.remove(robot)
+                    break
+
+                selected_node = dict_of_robot_shortest_path[robot][0]
+
+                if selected_node in nodes_occupied:
+
+                    print("Node occupied")
+                
+                else:
+
+                    nodes_occupied.append(selected_node)
+                    mqtt_payload = payload(robot,selected_node)
+                    client.publish("%s/robot/task"%robot, mqtt_payload)
+                    robot_shortest_path = dict_of_robot_shortest_path[robot]
+                    robot_shortest_path.remove(selected_node)
+
+                time.sleep(5)
+
+            nodes_occupied.clear()
+
