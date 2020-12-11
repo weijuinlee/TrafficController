6f7073cba067402b6aa43c0a6e6aa102f9d6fe58
weijuinlee <leewj96@gmail.com>
2020-12-11 17:31:16 +0800

    change port

--- app.py
+++ app.py
@@ -25,8 +25,15 @@ def get_patrol():
     patrol_task(data_json)
     return "Finished"
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+	# Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.debug = True
+    app.run(host='0.0.0.0', port=port)
+
 
 # @app.route("/get_word_count_result/<job_key>", methods=['GET'])
 # def get_word_count_result(job_key):
