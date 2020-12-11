11b63d0babb2615f0bd329b8f9a20097fe15aa41
weijuinlee <leewj96@gmail.com>
2020-12-11 17:21:03 +0800

    remove redis

--- app.py
+++ app.py
@@ -1,119 +1,32 @@
-# # from rq.job import Job
-# # from flask_restful import reqparse
-# # from flask import Flask, abort, jsonify, request
-# # from traffic_control_func import patrol_task
-# # from redis_func import redis_conn, redis_queue
-# # from flask_cors import CORS
-
-# # app = Flask(__name__)
-# # cors = CORS(app)
-
-# # @app.errorhandler(404)
-# # def resource_not_found(exception):
-# #     return jsonify(error=str(exception)), 404
-
-# # @app.route("/")
-# # def home():
-# #     return "Queue is running!"
-
-# # @app.route("/enqueue", methods=["POST"])
-# # def enqueue():
-
-# #     if request.method == "POST":
-# #         input_data = request.json
-# #         job = redis_queue.enqueue(patrol_task,input_data,job_timeout=600)
-# #     return jsonify({"job_id": job.id})
-
-# # if __name__ == "__main__":
-# #     app.run(debug=True,host="0.0.0.0")
-
-# import os
-# import sys, logging
-# import json
-# from flask import Flask, jsonify
-# # from flask import render_template
-# from flask import request
-# from flask_cors import CORS
-
-# from rq import Queue
-# from rq.job import Job
-# from rq import get_current_job
-# from worker import conn
-
-# # from lib.word_counter import WordCounter
-# from traffic_control_func import patrol_task
-
-# q = Queue(connection=conn)
-# app = Flask(__name__)
-# cors = CORS(app)
-# # word_counter = WordCounter()
-
-# @app.errorhandler(404)
-# def resource_not_found(exception):
-#     return jsonify(error=str(exception)), 404
-
-# @app.route('/')
-# def main():
-#     return "Queue is running!"
-
-# # @app.route('/get_word_count', methods=['POST'])
-# # def get_word_count():
-# #     data_json = json.loads(request.data)
-# #     job = q.enqueue(word_counter.count_words, data_json["sentence"])
-# #     return job.key
-
-# @app.route("/enqueue", methods=["POST"])
-# def enqueue():
-
-#     if request.method == "POST":
-#         input_data = request.json
-#         job = q.enqueue(patrol_task,input_data,job_timeout=600)
-#     return jsonify({"job_id": job.id})
-
-# # @app.route("/get_word_count_result/<job_key>", methods=['GET'])
-# # def get_word_count_result(job_key):
-# #     job_key = job_key.replace("rq:job:", "")
-# #     job = Job.fetch(job_key, connection=conn)
-
-# #     if(not job.is_finished):
-# #         return "Not yet", 202
-# #     else:
-# #         return str(job.result), 200
-
-# if __name__ == '__main__':
-# 	# Bind to PORT if defined, otherwise default to 5000.
-#     port = int(os.environ.get('PORT', 5000))
-#     app.debug = True
-#     app.run(host='0.0.0.0', port=port)
-
 import os
 import sys, logging
 import json
 from flask import Flask
 # from flask import render_template
 from flask import request
-
-from rq import Queue
-from rq.job import Job
-from rq import get_current_job
-from worker import conn
-
+import json
+import argparse
+from flask import Flask, request, jsonify, make_response
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from traffic_control_func import patrol_task
-# from trafficController import WordCounter
+from flask_cors import CORS
 
-q = Queue(connection=conn)
 app = Flask(__name__)
-# word_counter = WordCounter()
+api = Api(app)
+cors = CORS(app)
 
 @app.route('/')
 def main():
     return "Queue is running!"
 
 @app.route('/enqueue', methods=['POST'])
-def get_word_count():
+def get_patrol():
     data_json = json.loads(request.data)
-    job = q.enqueue(patrol_task, data_json)
-    return job.key
+    patrol_task(data_json)
+    return "Finished"
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 # @app.route("/get_word_count_result/<job_key>", methods=['GET'])
 # def get_word_count_result(job_key):
@@ -125,8 +38,43 @@ def get_word_count():
 #     else:
 #         return str(job.result), 200
 
-if __name__ == '__main__':
-	# Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.debug = True
-    app.run(host='0.0.0.0', port=port)
+# if __name__ == '__main__':
+# 	# Bind to PORT if defined, otherwise default to 5000.
+#     port = int(os.environ.get('PORT', 5000))
+#     app.debug = True
+#     app.run(host='0.0.0.0', port=port)
+
+# from flask import Flask  
+# from filmService import film_api  
+  
+# app = Flask(__name__)  
+# app.register_blueprint(film_api, url_prefix='/film_api')  
+ 
+# @app.route("/")  
+# def service():  
+#     return "Service is running!"  
+  
+# if __name__ == "__main__":  
+#     app.run()  
+
+# from flask import Flask, request
+# from flask_restful import Resource, Api
+# from json import dumps
+
+# app = Flask(__name__)
+# api = Api(app)
+
+# api.add_resource(Departments_Meta, '/departments')  # bind url identifier to class
+# api.add_resource(Departmental_Salary, '/dept/<string:department_name>')  # bind url identifier to class; also make it querable
+# api.add_resource(multiply, '/multiply/<int:number>')  # whatever the number is, multiply by 2
+
+
+# from flask import Flask
+# app = Flask(__name__)
+
+# @app.route('/patrol')
+# def patrol_task():
+#     patrol_task
+
+# if __name__ == '__main__':
+#     app.run()
