7296ad431388865363c195a5fcf07331b743a314
weijuinlee <leewj96@gmail.com>
2020-12-11 13:29:31 +0800

    add heroku redis

--- redis_func.py
+++ redis_func.py
@@ -1,12 +1,12 @@
 import os
-
+import urlparse
 import redis
 from rq import Queue
 
 redis_conn = redis.Redis(
-    host=os.getenv("REDIS_HOST", "127.0.0.1"),
-    port=os.getenv("REDIS_PORT", "6379"),
-    password=os.getenv("REDIS_PASSWORD", ""),
+    host=os.getenv("REDIS_HOST", "redis://default:T0yW5gl8JejxBbc6k2z4EErSpArFpDtO@redis-18286.c233.eu-west-1-1.ec2.cloud.redislabs.com"),
+    port=os.getenv("REDIS_PORT", "18286"),
+    password=os.getenv("REDIS_PASSWORD", "T0yW5gl8JejxBbc6k2z4EErSpArFpDtO"),
 )
 
 redis_queue = Queue(connection=redis_conn)
