b98eeb126d175951538b9004f0a0ff869fe0faaa
weijuinlee <leewj96@gmail.com>
2021-01-12 21:47:45 +0800

    added redis

--- app.py
+++ app.py
@@ -2,25 +2,61 @@ import json
 from flask import Flask, jsonify, abort, make_response,request
 from traffic_control_func import patrol_task
 from flask_cors import CORS
+from redis_func import redis_conn, redis_queue
+# from multiprocessing import Process
+
+# app = Flask(__name__)
+# cors = CORS(app)
+
+# @app.route('/')
+# def main():
+#     return "App is running!"
+
+# @app.route('/schedule/task', methods=['POST'])
+# def create_task():
+#     if not request.json:
+#         about(400)
+#     data = request.json
+#     patrol_task(data)
+#     return "Task completed."
+
+# @app.route("/task", methods=['POST'])
+# def task():
+#     print(request.json)
+#     body_job_id = request.json.get("job_id")
+#     body_operator = request.json.get("operator")
+#     if is_valid_request(body_job_id, body_operator) == False:
+#         return "Bad request, invalid job_id: %s or operator: %s" % (body_job_id, body_operator), 401
+
+#     task_cb = Process(target=run_task, args=( body_job_id, body_operator))
+#     task_cb.start()
+#     return "Job_id received!", 202
+
+# @app.errorhandler(404)
+# def not_found(error):
+#     return make_response(jsonify({'error': 'Not found'}), 404)
+
+# if __name__ == '__main__':
+# 	app.run(debug=True,host="0.0.0.0", threaded=True)
 
 app = Flask(__name__)
 cors = CORS(app)
 
-@app.route('/')
-def main():
-    return "App is running!"
-
-@app.route('/schedule/task', methods=['POST'])
-def create_task():
-    if not request.json:
-        about(400)
-    data = request.json
-    patrol_task(data)
-    return "Task completed."
-
 @app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+def resource_not_found(exception):
+    return jsonify(error=str(exception)), 404
 
-if __name__ == '__main__':
-	app.run(debug=True,host="0.0.0.0")
+@app.route("/")
+def home():
+    return "Queue is running!"
+
+@app.route("/enqueue", methods=["POST"])
+def enqueue():
+
+    if request.method == "POST":
+        input_data = request.json
+        job = redis_queue.enqueue(patrol_task(input_data),job_timeout=100000)
+    return jsonify({"job_id": job.id})
+
+if __name__ == "__main__":
+    app.run(debug=True,host="0.0.0.0")
